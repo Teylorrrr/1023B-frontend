4ab4b0d9af5e604b1b07c8f0a51cf91cf74a4e96
tyler the coder <125413796+Teylorrrr@users.noreply.github.com>
2025-06-09 12:28:50 -0300

    d
    w

---
+++ src/Container.tsx
@@ -0,0 +1,17 @@
+/// compopnente em sua ciração 
+/// states-> estados salvam estados de variavieis do react e quando esses estados mudom automaticamente o react ertende que ele precisa atualizar a pagina para mostrar os novos valores do componente
+
+/*
+function Container() {
+  return (
+    <h1>header</h1>
+  )
+}*/
+import React from 'react'
+class Container extends React.Component {
+
+  render(): React.ReactNode {
+    return  <h1>yhjjhkkkkkkkkkkkkkkk</h1>
+  }
+}
+export default Container
--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
-import Header from './Header.tsx'
-import Body from './Body.tsx'
-import Footer from './Footer.tsx'
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Header />
-       <Body />
-       <Footer />
-=======
-// import './index.css'
-import Header from './Header.tsx'
-import Body from './Body.tsx'
-import Footer from './Footer.tsx'
+      
+import Container from './Container.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Header />
-    <Body></Body>
-    <Footer></Footer>
->>>>>>> c9e89bfee27423f69d5bde0a5cfc589880fbb112
+    <Container />
   </StrictMode>,
 )
